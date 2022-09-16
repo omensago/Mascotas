5a525f60e9f9b9d180440fd21b7e6ad18aea7345
Jonathan Rincon <Jonathanandres080@gmail.com>
2022-09-15 21:58:40 -0500

    Cambios que no sirven

---
+++ MascotaFeliz.App.Frontend/Pages/Duenos/DetallesDuenos.cshtml.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MascotaFeliz.App.Dominio;
+using MascotaFeliz.App.Persistencia;
+
+namespace MascotaFeliz.App.Frontend.Pages
+{
+    public class DetallesDuenosModel : PageModel
+    {
+        private readonly IRepositorioDueno _repoDueno;
+        public Dueno dueno { get; set; }
+
+        public DetallesDuenosModel()
+        {
+            this._repoDueno = new RepositorioDueno(new Persistencia.AppContext());
+        }
+        public IActionResult OnGet(int duenoId)
+        {
+            {
+                dueno = _repoDueno.GetDueno(duenoId);
+                if (dueno == null)
+                {
+                    return RedirectToPage("./NotFound");
+                }
+                else
+                {
+                    return Page();
+                }
+
+            }
+        }
+    }
+}
