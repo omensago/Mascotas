075c182144dec77128898824e6b9ae7687a44097
Jonathan Rincon <Jonathanandres080@gmail.com>
2022-09-16 23:44:15 -0500

    commit de agregar, editar y mostrar mascota 3

---
+++ MascotaFeliz.App.Frontend/Pages/Mascotas/EditarMascota.cshtml.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MascotaFeliz.App.Dominio;
+using MascotaFeliz.App.Persistencia;
+
+namespace MascotaFeliz.App.Frontend.Pages
+{
+    public class EditarMascotaModel : PageModel
+    {
+        private readonly IRepositorioMascota _repoMascota;
+        [BindProperty]
+        public Mascota mascota { get; set; }
+
+        public EditarMascotaModel(){
+            this._repoMascota = new RepositorioMascota( new Persistencia.AppContext()); 
+        }
+
+        public IActionResult OnGet(int? mascotaId){
+            if (mascotaId.HasValue){
+                mascota = _repoMascota.GetMascota(mascotaId.Value);
+            }
+            else{
+                mascota = new Mascota();
+            }
+            if (mascota == null){
+                return RedirectToPage("./NotFound");
+            }
+            else{
+                return Page();
+            }
+                
+        }
+        public IActionResult OnPost(){
+            if (!ModelState.IsValid){
+                return Page();
+            }
+            if(mascota.Id > 0){
+                mascota = _repoMascota.UpdateMascota(mascota);
+            }
+            else{
+                _repoMascota.AddMascota(mascota);
+            }
+            return Page();
+        }
+    }
+}
