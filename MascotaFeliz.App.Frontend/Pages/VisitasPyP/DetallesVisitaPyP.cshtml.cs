2d7d61e324cbb17eb70c7dc22302d7c822ab9a6f
Jonathan Rincon <Jonathanandres080@gmail.com>
2022-09-21 19:55:30 -0500

    commit creaccion de visitas P Y P v3

---
+++ MascotaFeliz.App.Frontend/Pages/VisitasPyP/DetallesVisitaPyP.cshtml.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MascotaFeliz.App.Frontend.Pages
+{
+    public class DetallesVisitaPyPModel : PageModel
+    {
+        /*
+        public class DetallesVeterinariosModel : PageModel
+        {
+            private readonly IRepositorioVeterinario _repoVeterinario;
+            public Veterinario veterinario { get; set; }
+            public DetallesVeterinariosModel()
+            {
+                this._repoVeterinario = new RepositorioVeterinario(new Persistencia.AppContext());
+            }
+            public IActionResult OnGet(int veterinarioId)
+            {
+                veterinario = _repoVeterinario.GetVeterinario(veterinarioId);
+                if (veterinario == null)
+                {
+
+                    return RedirectToPage("./NotFounSd");
+                }
+                else
+                {
+                    return Page();
+                }
+            }
+        }*/
+    }
+}
+
