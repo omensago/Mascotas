c071f3db74c1187504864af632d6f6f4dc30eea4
Jonathan Rincon <Jonathanandres080@gmail.com>
2022-09-22 23:25:09 -0500

    commit visita solucionado

--- MascotaFeliz.App.Frontend/Pages/VisitasPyP/DetallesVisitaPyP.cshtml.cs
+++ MascotaFeliz.App.Frontend/Pages/VisitasPyP/DetallesVisitaPyP.cshtml.cs
@@ -4,34 +4,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MascotaFeliz.App.Dominio;
+using MascotaFeliz.App.Persistencia;
 
 namespace MascotaFeliz.App.Frontend.Pages
 {
+
+
     public class DetallesVisitaPyPModel : PageModel
     {
-        /*
-        public class DetallesVeterinariosModel : PageModel
+        private readonly IRepositorioVisitaPyP _repoVisitaPyP;
+        public VisitaPyP visitaPyP { get; set; }
+        public DetallesVisitaPyPModel()
         {
-            private readonly IRepositorioVeterinario _repoVeterinario;
-            public Veterinario veterinario { get; set; }
-            public DetallesVeterinariosModel()
+            this._repoVisitaPyP = new RepositorioVisitaPyP(new Persistencia.AppContext());
+        }
+        public IActionResult OnGet(int visitaPyPId)
+        {
+            visitaPyP = _repoVisitaPyP.GetVisitaPyP(visitaPyPId);
+            if (visitaPyP == null)
             {
-                this._repoVeterinario = new RepositorioVeterinario(new Persistencia.AppContext());
-            }
-            public IActionResult OnGet(int veterinarioId)
-            {
-                veterinario = _repoVeterinario.GetVeterinario(veterinarioId);
-                if (veterinario == null)
-                {
 
-                    return RedirectToPage("./NotFounSd");
-                }
-                else
-                {
-                    return Page();
-                }
+                return RedirectToPage("./NotFounSd");
             }
-        }*/
+            else
+            {
+                return Page();
+            }
+        }
     }
-}
 
+}
